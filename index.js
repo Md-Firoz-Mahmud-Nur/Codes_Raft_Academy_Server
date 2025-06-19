13d2bf4fb0c3a42fd41b9592dc344414e5ee7a47
firoz.nur <firoznur5@gmail.com>
2025-06-19 10:05:19 +0600

    Deploy : Vercel

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // Middleware
 app.use(
-  cors({
-    origin: ["http://localhost:5173"],
-    credentials: true,
-  })
+  cors(
+  //   {
+  //   origin: ["http://localhost:5173"],
+  //   credentials: true,
+  // }
+)
 );
 app.use(express.json());
 
